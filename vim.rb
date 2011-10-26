c5ff7f5bea5c2801fe8ab02a1688e525bd6d57a4
Jonathan Mukai & Peter Jaros <pair+jmukai+pjaros@pivotallabs.com>
2011-10-26 14:53:34 -0400

    Configure vim under rvm system.

--- vim.rb
+++ vim.rb
@@ -13,17 +13,16 @@ class Vim < Formula
     # Debugging symbols
     ENV["CFLAGS"] = "-g"
 
-    system "./configure", "--prefix=#{prefix}",
-                          "--mandir=#{man}",
-                          "--enable-gui=no",
-                          "--without-x",
-                          "--disable-nls",
-                          "--enable-multibyte",
-                          "--with-tlib=ncurses",
-                          "--enable-pythoninterp",
-                          "--enable-rubyinterp",
-                          "--with-ruby-command=/usr/bin/ruby",
-                          "--with-features=huge"
+    system "rvm", "system", "exec", "./configure", "--prefix=#{prefix}",
+                                                   "--mandir=#{man}",
+                                                   "--enable-gui=no",
+                                                   "--without-x",
+                                                   "--disable-nls",
+                                                   "--enable-multibyte",
+                                                   "--with-tlib=ncurses",
+                                                   "--enable-pythoninterp",
+                                                   "--enable-rubyinterp",
+                                                   "--with-features=huge"
     system "make"
     system "make install"
   end
